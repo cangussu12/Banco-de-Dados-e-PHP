8c2da8d67cb35fc3f76a531042ad1176f143f3d7
cangussu12 <leo.tywin@gmail.com>
2020-08-22 16:49:16 -0300

    Banco de dados e PHP

--- grouporderlimit.php
+++ grouporderlimit.php
@@ -1,8 +1,9 @@
 <?php
     
+    
     $pdo = new PDO('mysql:host=localhost;dbname=phpbasico','root','');
 
-    $sql = $pdo->prepare("SELECT * FROM `clientes` ORDER BY nome DESC LIMIT 3");
+    $sql = $pdo->prepare("SELECT * FROM `clientes` LEFT JOIN cargos ON `clientes`.`cargo` = `cargos`.`id`");
     /* ASC - Crescente e DESC Descrecente*/
     $sql->execute();
     $clientes = $sql->fetchAll();
@@ -10,6 +11,8 @@
     foreach($clientes as $key => $value) {
 
         echo $value['nome'];
+        echo ' | ';
+        echo $value['nome_cargo'];
         echo '<hr>';    
     }
 
